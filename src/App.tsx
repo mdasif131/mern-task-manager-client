fbee1df7892d93359a9aa89970ba4fd0db6ead5a
mdasif131 <asifmdasifchy@gmail.com>
2026-01-01 04:11:38 +0600

    initial file setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <Button>Click Me!</Button>
+    </div>
+  );
+}
+
+export default App
