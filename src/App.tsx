b311de12bbbc8a2b2dc9e19f4931e49f576041fa
mdasif131 <asifmdasifchy@gmail.com>
2026-01-01 18:13:46 +0600

    simple route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { Button } from "./components/ui/button";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CompeletedPage from './pages/CompeletedPage';
+import CreatePage from './pages/CreatePage';
+import DashBoardPage from './pages/DashBoardPage';
+import NewPage from './pages/NewPage';
+import ProfilePage from './pages/ProfilePage';
+import ProgressPage from './pages/ProgressPage';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import ForgetPssPage from './pages/ForgetPssPage';
+import Page404 from './pages/Page404';
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-      <Button>Click Me!</Button>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashBoardPage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/all" element={<NewPage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+          <Route path="/completed" element={<CompeletedPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="/forget-password" element={<ForgetPssPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
-export default App
+export default App;
