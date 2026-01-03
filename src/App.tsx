f4c6700bbdc62c0823e47497729a46d138733c5a
mdasif131 <asifmdasifchy@gmail.com>
2026-01-03 19:07:21 +0600

    Route Security setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import FullScreenLoader from './components/masterLayout/FullScreenLoader';
 import CompeletedPage from './pages/CompeletedPage';
 import CreatePage from './pages/CreatePage';
@@ -11,27 +11,43 @@ import ProfilePage from './pages/ProfilePage';
 import ProgressPage from './pages/ProgressPage';
 import RegistrationPage from './pages/RegistrationPage';
 import CanceledPage from './pages/CanceledPage';
+import { getToken } from './helper/sessionHelper';
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DashBoardPage />} />
-          <Route path="/create" element={<CreatePage />} />
-          <Route path="/all" element={<NewPage />} />
-          <Route path="/progress" element={<ProgressPage />} />
-          <Route path="/completed" element={<CompeletedPage />} />
-          <Route path="/canceled" element={<CanceledPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/forget-password" element={<ForgetPssPage />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-      </BrowserRouter>
-      <FullScreenLoader />
-    </>
-  );
+  if (getToken()) {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<DashBoardPage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/all" element={<NewPage />} />
+            <Route path="/progress" element={<ProgressPage />} />
+            <Route path="/completed" element={<CompeletedPage />} />
+            <Route path="/canceled" element={<CanceledPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </BrowserRouter>
+        <FullScreenLoader />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            <Route path="/forget-password" element={<ForgetPssPage />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </BrowserRouter>
+        <FullScreenLoader />
+      </>
+    );
+  }
+  
 };
 
 export default App;
